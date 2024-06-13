cc48c92805d4048da2330686dd2f40cc66ac60e3
Eti Gal-Or <etim139@gmail.com>
2024-06-13 15:44:24 +0300

    styled folders and files, add mime types icons

--- src/FilePreview.jsx
+++ src/FilePreview.jsx
@@ -1,29 +1,39 @@
 import { NavLink } from 'react-router-dom'
-import { FaFolder, FaRegFilePdf, FaRegFileWord, FaRegFileExcel, FaRegFilePowerpoint } from 'react-icons/fa'
+import {
+  FaFolder,
+  FaRegFilePdf,
+  FaRegFileWord,
+  FaRegFileExcel,
+  FaRegFilePowerpoint,
+} from 'react-icons/fa'
+import { AiOutlineFileJpg } from "react-icons/ai";
 import { useState, useEffect } from 'react'
 
 export default function FilePreview({ fileName, pathname }) {
-    const [fileType, setFileType] = useState('')
+  const [fileType, setFileType] = useState('')
 
-    useEffect(() => {
-        if (fileName.includes('.')) setFileType(fileName.split('.')[1])
-    }, [])
-    
+  useEffect(() => {
+    if (fileName.includes('.')) setFileType(fileName.split('.')[1])
+        else setFileType('folder')
+  }, [])
 
   // I build the pathname in a way that he'll look like the structure in the root/public folder in the server - then I send it with axios
   return (
     <NavLink
       className="file-preview"
       key={fileName}
-      to={`${pathname}${pathname === '/' ? fileName : '/' + fileName}`}
+      to={`${pathname}${pathname === '/' ? fileName : '/' + fileName}`} 
     >
-      {fileType === '' && <FaFolder />}
-      {fileType === 'pdf' && <FaRegFilePdf />}
-      {fileType === 'docx' && <FaRegFileWord />}
-      {fileType === 'xlsx' && <FaRegFileExcel />}
-      {fileType === 'pptx' && <FaRegFilePowerpoint />}
-
-      {fileName}
+      <span className={`file-preview-icon ${fileType}`}>
+        {fileType === 'folder' && <FaFolder />}
+        {fileType === 'pdf' && <FaRegFilePdf />}
+        {fileType === 'docx' && <FaRegFileWord />}
+        {fileType === 'xlsx' && <FaRegFileExcel />}
+        {fileType === 'pptx' && <FaRegFilePowerpoint />}
+        {fileType === 'jpg' && <AiOutlineFileJpg />
+}
+      </span>
+      <span className="file-preview-name">{fileName}</span>
     </NavLink>
   )
 }
