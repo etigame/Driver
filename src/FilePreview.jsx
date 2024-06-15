1f391beebcda6669ca262dca2f1b40e95c6dea8d
Eti Gal-Or <etim139@gmail.com>
2024-06-16 00:13:36 +0300

    add ability to rename files and directories

--- src/FilePreview.jsx
+++ src/FilePreview.jsx
@@ -10,15 +10,18 @@ import {
 } from 'react-icons/fa'
 import { AiOutlineFileJpg } from 'react-icons/ai'
 import { IoMdMore } from 'react-icons/io'
-import { IoInformationCircleOutline } from "react-icons/io5";
+import { IoInformationCircleOutline } from 'react-icons/io5'
 import { MdOutlineDriveFileRenameOutline } from 'react-icons/md'
 
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import Popup from './Popup'
 
 export default function FilePreview({ fileName, pathname, fetchFiles }) {
   const [fileType, setFileType] = useState('')
   const [isTooltipShown, setIsTooltipShown] = useState(false)
+  const [isPopupShown, setIsPopupShown] = useState(false)
+  const [popupContent, setPopupContent] = useState('')
 
   useEffect(() => {
     if (fileName.includes('.')) setFileType(fileName.split('.')[1])
@@ -30,68 +33,98 @@ export default function FilePreview({ fileName, pathname, fetchFiles }) {
     setIsTooltipShown((prev) => !prev)
   }
 
-  const handleDeleteFile = (e) => {
+  const handleDelete = (e) => {
     e.preventDefault()
-    axios.delete(`http://localhost:3000/file/?path=${pathname}/${fileName}`).then((res) => fetchFiles())
     setIsTooltipShown(false)
+    axios
+      .delete(`http://localhost:3000/file/?path=${pathname}/${fileName}`)
+      .then((res) => fetchFiles())
+  }
+
+  const handlePopup = (e) => {
+    e.preventDefault()
+    setIsTooltipShown(false)
+    setIsPopupShown(true)
+    setPopupContent('rename')
+  }
+
+  const handleRename = (e, newName) => {
+    // i send the old path via query params 
+    axios
+      .put(`http://localhost:3000/file/?path=${pathname}/${fileName}`, {
+        newPath: `${pathname}/${newName}`
+      })
+      .then((res) => fetchFiles())
+
+    setIsPopupShown(false)
   }
 
   // I build the pathname in a way that he'll look like the structure in the root/public folder in the server - then I send it with axios
   return (
-    <NavLink
-      className="file-preview"
-      key={fileName}
-      to={`${pathname}${pathname === '/' ? fileName : '/' + fileName}`}
-    >
-      <span className={`file-preview-icon ${fileType}`}>
-        {fileType === 'folder' && <FaFolder />}
-        {fileType === 'pdf' && <FaRegFilePdf />}
-        {fileType === 'docx' && <FaRegFileWord />}
-        {fileType === 'xlsx' && <FaRegFileExcel />}
-        {fileType === 'pptx' && <FaRegFilePowerpoint />}
-        {fileType === 'jpg' && <AiOutlineFileJpg />}
-      </span>
-
-      <div className="file-preview-bottom">
-        <span className="file-preview-name" title={fileName}>
-          {fileName}
+    <>
+      <NavLink
+        className="file-preview"
+        key={fileName}
+        to={`${pathname}${pathname === '/' ? fileName : '/' + fileName}`}
+      >
+        <span className={`file-preview-icon ${fileType}`}>
+          {fileType === 'folder' && <FaFolder />}
+          {fileType === 'pdf' && <FaRegFilePdf />}
+          {fileType === 'docx' && <FaRegFileWord />}
+          {fileType === 'xlsx' && <FaRegFileExcel />}
+          {fileType === 'pptx' && <FaRegFilePowerpoint />}
+          {fileType === 'jpg' && <AiOutlineFileJpg />}
         </span>
-        <span
-          className="file-preview-actions"
-          onClick={(e) => handleFileActions(e)}
-        >
-          <IoMdMore />
-        </span>
-      </div>
 
-      {isTooltipShown && (
-        <section className="file-actions-tooltip">
-          <div className='action'>
-            <span>
-              <IoInformationCircleOutline />
-            </span>
-            Details
-          </div>
-          <div className='action'>
-            <span>
-              <FaCloudDownloadAlt />
-            </span>
-            Download
-          </div>
-          <div className='action'>
-            <span>
-              <MdOutlineDriveFileRenameOutline />
-            </span>
-            Rename
-          </div>
-          <div className='action' onClick={handleDeleteFile}>
-            <span>
-              <FaRegTrashAlt />
-            </span>
-            Remove
-          </div>
-        </section>
+        <div className="file-preview-bottom">
+          <span className="file-preview-name" title={fileName}>
+            {fileName}
+          </span>
+          <span
+            className="file-preview-actions"
+            onClick={(e) => handleFileActions(e)}
+          >
+            <IoMdMore />
+          </span>
+        </div>
+
+        {isTooltipShown && (
+          <section className="file-actions-tooltip">
+            <div className="action">
+              <span className='action-icon'>
+                <IoInformationCircleOutline />
+              </span>
+              <span className="action-name">Details</span>
+            </div>
+            <div className="action">
+              <span className='action-icon'>
+                <FaCloudDownloadAlt />
+              </span>
+              <span className="action-name">Download</span>
+            </div>
+            <div className="action" onClick={handlePopup}>
+              <span className='action-icon'>
+                <MdOutlineDriveFileRenameOutline />
+              </span>
+              <span className="action-name">Rename</span>
+            </div>
+            <div className="action" onClick={handleDelete}>
+              <span className='action-icon'>
+                <FaRegTrashAlt />
+              </span>
+              <span className="action-name">Remove</span>
+            </div>
+          </section>
+        )}
+      </NavLink>
+      {isPopupShown && (
+        <Popup
+          popupContent={popupContent}
+          setIsPopupShown={setIsPopupShown}
+          fileName={fileName}
+          handleRename={handleRename}
+        />
       )}
-    </NavLink>
+    </>
   )
 }
--- src/Popup.jsx
+++ src/Popup.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 
-export default function Popup({ popupContent, setIsPopupShown, handleNewFolder }) {
-  const [folderName, setFolderName] = useState('')
+export default function Popup({
+  popupContent,
+  setIsPopupShown,
+  handleNewFolder,
+  fileName, handleRename
+}) {
+  const [folderName, setFolderName] = useState('') // to create new folder
+  const [newName, setNewName] = useState(fileName) // to rename file or dir
 
   return (
     <section className="popup-overlay">
       <section className="popup">
-        <button className='close' onClick={() => setIsPopupShown(false)}>&times;</button>
+        <button className="close" onClick={() => setIsPopupShown(false)}>
+          &times;
+        </button>
         {popupContent === 'newFolder' && (
           <form onSubmit={(e) => handleNewFolder(e, folderName)}>
             <label htmlFor="folderName">Folder Name</label>
@@ -18,6 +26,20 @@ export default function Popup({ popupContent, setIsPopupShown, handleNewFolder }
               onInput={(e) => setFolderName(e.target.value)}
             />
             <button type="submit">Create Folder</button>
+          </form>
+        )}
+
+        {popupContent === 'rename' && (
+          <form onSubmit={(e) => handleRename(e, newName)}>
+            <label htmlFor="newName">Rename</label>
+            <input
+              type="text"
+              name="newName"
+              id="newName"
+              value={newName}
+              onInput={(e) => setNewName(e.target.value)}
+            />
+            <button type="submit">Rename</button>
           </form>
         )}
       </section>
